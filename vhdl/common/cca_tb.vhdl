9268647a02df6c785d308171f7d93e32ade84735
Min <63823931+MuteMini@users.noreply.github.com>
2024-04-18 18:46:00 -0400

    fixed adder testbench

--- vhdl/common/cca_tb.vhdl
+++ vhdl/common/cca_tb.vhdl
@@ -17,30 +17,31 @@ architecture behav of cca_tb is
         );
     end component;
 
-    for cca_2_bit : cancel_carry_adder use entity work.cancel_carry_adder;
+    for cca_3bit : cancel_carry_adder use entity work.cancel_carry_adder;
+    for cca_5bit : cancel_carry_adder use entity work.cancel_carry_adder;
 
-    signal a_3bit, b_3bit, out_3bit : unsigned(2 downto 0);
-    signal a_5bit, b_5bit, out_5bit : unsigned(4 downto 0);
+    signal a_3bit, b_3bit, out_3bit : std_logic_vector(2 downto 0);
+    signal a_5bit, b_5bit, out_5bit : std_logic_vector(4 downto 0);
 begin
     cca_3bit : cancel_carry_adder
-        generic map (adder_width => 2)
-        port map (  a=>std_logic_vector(a_3bit), 
-                    b=>std_logic_vector(b_3bit), 
-                    output=>std_logic_vector(out_3bit) );
+        generic map (adder_width => 3)
+        port map (  a=>a_3bit, 
+                    b=>b_3bit, 
+                    output=>out_3bit );
 
     cca_5bit : cancel_carry_adder
         generic map (adder_width => 5)
-        port map (  a=>std_logic_vector(a_5bit), 
-                    b=>std_logic_vector(b_5bit), 
-                    output=>std_logic_vector(out_5bit) );
+        port map (  a=>a_5bit, 
+                    b=>b_5bit, 
+                    output=>out_5bit );
 
     process
         type array_3bit_io is record
-            a, b, o : unsigned(2 downto 0);
+            a, b, o : std_logic_vector(2 downto 0);
         end record;
         
         type array_5bit_io is record
-            a, b, o : unsigned(3 downto 0);
+            a, b, o : std_logic_vector(4 downto 0);
         end record;
 
         type add_3bit_io_array is array (natural range <>) of array_3bit_io;
@@ -50,16 +51,16 @@ begin
              ("101", "001", "110"),
              ("011", "011", "110"),
              ("111", "001", "000"),
-             ("111", "110", "100"));
+             ("111", "110", "101"));
 
         type add_5bit_io_array is array (natural range <>) of array_5bit_io;
         constant cca_5bit_io : add_5bit_io_array :=
             (("00000", "00000", "00000"),
              ("00001", "00001", "00010"),
-             ("01101", "10001", "11101"),
+             ("01101", "10001", "11110"),
              ("01010", "01100", "10110"),
              ("11101", "00011", "00000"),
-             ("11010", "11011", "00101"));
+             ("11010", "11011", "10101"));
 
     begin
         for i in cca_3bit_io'range loop
@@ -72,11 +73,11 @@ begin
         end loop;
 
         for i in cca_5bit_io'range loop
-            a_3bit <= cca_5bit_io(i).a;
-            b_3bit <= cca_5bit_io(i).b;
+            a_5bit <= cca_5bit_io(i).a;
+            b_5bit <= cca_5bit_io(i).b;
 
             wait for 1 ns;
-            assert out_3bit = cca_5bit_io(i).o
+            assert out_5bit = cca_5bit_io(i).o
                 report "bad sum value" severity error;
         end loop;  
         wait;
